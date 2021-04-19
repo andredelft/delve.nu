5632cabb053f76cd6d7822cb037b0959ca717b86
André van Delft <andrevandelft@outlook.com>
2021-04-19 15:29:35 +0200

    Convert the homepage into a portfolio

--- delve/views.py
+++ delve/views.py
@@ -1,4 +1,47 @@
 from django.shortcuts import render
 
+PORTFOLIO_ITEMS = [
+    {
+        'name': 'scholarly_editions',
+        'label': 'Brill\'s Scholarly Editions',
+        'href': 'https://dh.brill.com/scholarlyeditions',
+        'dark_mode': True,
+        'no_slides': 3,
+    },
+    {
+        'name': 'vincent_jacob',
+        'label': 'Vincent Jacob',
+        'href': 'https://vincentjacobmusic.com',
+        'no_slides': 3,
+    },
+    {
+        'name': 'nco',
+        'label': 'Natuurkundige Commissie Archives Online',
+        'href': 'https://dh.brill.com/nco',
+        'dark_mode': True,
+        'no_slides': 2,
+    },
+    {
+        'name': 'levensfilosofie',
+        'label': 'Colloquium Levensfilosofie',
+        'href': 'https://levensfilosofie.nu',
+        'dark_mode': True,
+        'no_slides': 3,
+    }
+]
+
+
+def _get_slide_names(name, no_slides):
+    return [f'images/{name}_{i + 1}.png' for i in range(no_slides)]
+
+
 def home(request):
-    return render(request, 'index.html')
+    portfolio_items = PORTFOLIO_ITEMS
+    portfolio_items = [
+        {**item, 'slides': _get_slide_names(item['name'], item['no_slides'])}
+        for item in portfolio_items
+    ]
+    print(portfolio_items)
+    return render(
+        request, 'index.html', context={'portfolio_items': portfolio_items}
+    )
